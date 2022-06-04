218bd29705d9b340c2fd8cbb0fdba5f7f590d276
BroJayAndrews <105384448+BroJayAndrews@users.noreply.github.com>
2022-06-03 20:05:09 -0400

    Update card.py

--- game/card.py
+++ game/card.py
@@ -1,14 +1,27 @@
 import random
 
 class Card:
+     """
+    class for flipping cards
+    """
     def __init__(self):
+          """
+        Allocated card variables
+        """
         self.score = 0
         self.card1 = 0
         self.card2 = 0
-    def firstcardflip():
+    def firstcardflip(): 
+        """
+        The first flip
+        """
+        
         Card.card1 = random.randint(1,13)
         return Card.card1
     def secondcardflip(card,hilo):
+           """
+        The Second flip
+        """
         Card.card2 = random.randint(1,13)
         print(f'The Second Card Is {Card.card2}')
         if card < Card.card2 and hilo == "high":
